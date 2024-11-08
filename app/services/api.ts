867349a2582bf24f885c28fe5a0e756cc437b5a2
Sayed <sayed@fourminutemedicine.com>
2024-11-08 21:05:04 +0200

    update api url

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5005/api';
+const API_BASE_URL = 'https://fourmm-llama-hackathon-backend.onrender.com/api';
 
 export const chatApi = {
   // Create a new conversation
